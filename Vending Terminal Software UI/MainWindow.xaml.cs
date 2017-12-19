8fb2d94a1a59f494004e5ad58870ef5959986fd8
Kochki <34690125+Kochki@users.noreply.github.com>
2017-12-19 21:46:21 +0300

    interface

---
+++ Vending Terminal Software UI/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using Vending_Terminal_Software_Classes;
+using Vending_Terminal_Software_UI.PagesFolder;
+
+namespace Vending_Terminal_Software_UI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            frameMain.Navigate(Pages.VM);
+        }
+    }
+}
---
+++ Vending Terminal Software UI/PagesFolder/Pages.cs
@@ -0,0 +1,12 @@
+﻿namespace Vending_Terminal_Software_UI.PagesFolder
+{
+    static class Pages
+    {
+        private static VM vM = new VM();
+
+        public static VM VM
+        {
+            get { return vM; }
+        }
+    }
+}
